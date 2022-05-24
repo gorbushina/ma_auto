3beb5a08c9a6c57d19cb78b0b561c16d03aec86e
Ekaterina <e.gorbushina@cdek.ru>
2022-05-25 00:03:11 +0500

    test 2

--- test_main.py
+++ test_main.py
@@ -1,11 +1,19 @@
 class MainClass:
+    __class_number = 20
+
 
     def get_local_number(self):
         return 14
 
+    def get_class_number(self):
+        return self.__class_number
+
 
 class TestMainClass:
+    main_class = MainClass()
 
     def test_get_local_number(self):
-        main_class = MainClass()
-        assert 14 == main_class.get_local_number(), "Not Success"
+        assert 14 == self.main_class.get_local_number(), "Not Success"
+
+    def test_get_class_number(self):
+        assert self.main_class.get_class_number() > 45, "Not Success"
