0cc76f15c314455dadf75369e93391e4af0e31cf
Ekaterina <e.gorbushina@cdek.ru>
2022-05-25 17:16:12 +0500

    test 3

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 class MainClass:
     __class_number = 20
+    __class_string = 'Hello, word'
 
 
     def get_local_number(self):
@@ -7,6 +8,9 @@ class MainClass:
 
     def get_class_number(self):
         return self.__class_number
+
+    def get_class_string (self):
+        return self.__class_string
 
 
 class TestMainClass:
@@ -17,3 +21,6 @@ class TestMainClass:
 
     def test_get_class_number(self):
         assert self.main_class.get_class_number() > 45, "Not Success"
+
+    def test_get_class_string (self):
+        assert "hello" in self.main_class.get_class_string() or "Hello" in self.main_class.get_class_string(), "Not Success"
